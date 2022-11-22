f63e2ce66e665afa8d9204cd54a32543f861f30c
Yash K <yassshrk19@gmail.com>
2022-11-22 12:01:34 +0530

    Create School_system.py
    Add to ObjectOrientedPrg branch

---
+++ OOP/Example_Codes/School_system.py
@@ -0,0 +1,36 @@
+class School:
+    def __init__(self,university, location):
+        self.university = university 
+        self.location = location
+        self.num_students = 0
+        self.num_teachers = 0
+        self.project = ""
+        self.category = ""
+    
+    def Details(self, num_students, num_teachers):
+        self.num_students = num_students
+        self.num_teachers = num_teachers
+    
+    def Projects(self, project_name, category):
+        self.project = project_name
+        self.category = category
+
+
+XYZUniv = School("XYZ University", "Pune")
+XYZUniv.Details(60,10)
+XYZUniv.Projects("Solar Windmill", "Environmental Science")
+
+print(f"Universiy Name: {XYZUniv.university}\nLocation : {XYZUniv.location}")
+print(f"Number of Students: {XYZUniv.num_students}\nNumber of Teachers : {XYZUniv.num_teachers}")
+print(f"Qualified Project Name: {XYZUniv.project}\nProject Category : {XYZUniv.category}")
+
+
+
+#XYZProjects = School.Projects("Glowing Lantern", "Environmental Science")
+
+#print("University Name: "+XYZUniv.university)
+# print("University Location: "+XYZUniv.location)
+
+#print(XYZDetails)
+#print("Student details of "+XYZUniv.university+" :"+ XYZDetails)
+#print("Project Details of "+XYZUniv.university+" :"+ XYZProjects)
