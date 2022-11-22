14e3a6296adf0ad350b1ce0e47c3c959e08a638f
Yash K <yassshrk19@gmail.com>
2022-11-22 22:31:16 +0530

    Update ListOverlap.py
    Added random list generation for both lists

--- Exercises/ListOverlap.py
+++ Exercises/ListOverlap.py
@@ -1,15 +1,28 @@
 #Compare two randomly generated lists and return elements which occur in both lists
-
-def listOverlap(listA, listB): 
+import random
+def listOverlap(): 
     finalList = []
+    listA = []
+    listB = []
+    sizeRangeA = random.randint(1,10)
+    sizeRangeB = random.randint(1,10)    
+    #Generating random values for listA
+    for x in range(sizeRangeA):
+        listA.append(int(random.randint(1,100)))
+    #Generating random values for listA
+    for x in range(sizeRangeB):
+        listB.append(int(random.randint(1,100)))
+    #Generating elements which occurred twice or more
     for i in listA:
         for j in listB:
             if j == i:
                 finalList.append(j)
             else:
                 continue
-    return finalList
+    print(f'List A is :{listA}')
+    print(f'List B is :{listB}')
+    print(f'Occurences of Similar elements: {finalList}')
 
 a = [1,2,3]
 b = [1,5,6,7,3]
-print(listOverlap(a,b))
+print(listOverlap())
