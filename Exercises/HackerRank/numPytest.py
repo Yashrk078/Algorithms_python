e4df96aab6bd8727f4a0ae65c272eb62c24a4de3
Yash K <yassshrk19@gmail.com>
2022-11-23 14:22:51 +0530

    changes in new numpytest

--- Exercises/HackerRank/numPytest.py
+++ Exercises/HackerRank/numPytest.py
@@ -1,4 +1,16 @@
-import numpy as np
+'''
+You are given a space separated list of numbers.
+Your task is to print a reversed NumPy array with the element type float.
+Print the reverse NumPy array with type float.
+'''
 
-a = np.array([1,2,3,4,4])
-b = np.array(a, float)
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    b = numpy.array(arr,float)
+    return b[::-1]
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
